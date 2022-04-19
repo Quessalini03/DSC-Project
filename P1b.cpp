557359cea54d24c960bfbad7d1be9aea08c0678e
Quessalini03 <vuchauduyquang@gmail.com>
2022-04-19 15:45:55 +0700

    add function bool bracesIsBalanced()

--- P1b.cpp
+++ P1b.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+
+bool bracesIsBalanced(string str);
+
+int main()
+{
+	string str;
+	getline(cin, str);
+	// ...
+	return 0;
+}
+
+bool bracesIsBalanced(string str) //  )((
+{
+	stack<char> cStack;
+	for (int i = 0; str[i]; i++) 
+	{
+		if (str[i] == '(' || str[i] == ')')
+		{
+			if (cStack.empty()) 
+			{
+				if (str[i] == ')') return 0;
+				else cStack.push( char(str[i]) );
+			}
+			else 
+			{
+				if (str[i] == ')') cStack.pop();
+				else cStack.push( char(str[i]) );
+			}
+		}
+	}
+	if (cStack.size() == 0) return 1;
+	else return 0;
+}
 
